1a0da85928a216939175271dc627ab24f085b503
Ross Bulat <ross@jkrbinvestments.com>
2022-02-17 13:10:46 +0000

    `None`

--- frame/election-provider-support/src/lib.rs
+++ frame/election-provider-support/src/lib.rs
@@ -379,7 +379,7 @@ pub trait SortedListProvider<AccountId> {
 	fn on_insert(id: AccountId, weight: VoteWeight) -> Result<(), Self::Error>;
 
 	/// Hook for updating a single id that returns whether a change was made,
-	/// or None if there is nothing to do on update.
+	/// or `None` if there is nothing to do on update.
 	fn on_update(id: &AccountId, weight: VoteWeight) -> Option<bool>;
 
 	/// Hook for removing am id from the list.
