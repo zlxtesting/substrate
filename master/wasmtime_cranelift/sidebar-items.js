c139566f6d4bc63948dd2419e3d4100bb111964f
paritytech-ci <devops-team@parity.io>
2022-06-24 12:14:44 +0000

    ___Updated docs for master___

--- master/wasmtime_cranelift/sidebar-items.js
+++ master/wasmtime_cranelift/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["builder",""]],"macro":[["for_each_libcall","Iterates through all `LibCall` members and all runtime exported functions."]],"struct":[["CompiledFunction","Compiled function: machine code body, jump table offsets, and unwind information."]]});
+initSidebarItems({"fn":[["builder",""]],"struct":[["CompiledFunction","Compiled function: machine code body, jump table offsets, and unwind information."]]});
